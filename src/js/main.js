23e94636184dc12881f200d564547781d02b08b9
nsandoya <natsandoya@gmail.com>
2024-02-06 18:02:16 -0500

    Offcanvas Menu: Desarrollo desde 0 e implementación | Descarte de todo el contenido bootstrap, excepto los íconos

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+const offcanvasOpenBtn = document.querySelector(".burger-menu");
+const offcanvasCloseBtn = document.querySelector(".btn-close");
+const offcanvasItem = document.querySelector("ul");
+
+const offcanvasMenu = document.querySelector(".offcanvas-menu");
+
+offcanvasOpenBtn.addEventListener('click', (event)=>{
+    offcanvasMenu.classList.add('active')
+})
+
+offcanvasCloseBtn.addEventListener('click', (event)=>{
+    offcanvasMenu.classList.remove('active')
+})
+offcanvasItem.addEventListener('click', (event)=>{
+    offcanvasMenu.classList.remove('active')
+})
