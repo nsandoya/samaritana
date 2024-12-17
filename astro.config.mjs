0cf3249a77319764ef43ce296d7a621cf9ae2181
nsandoya <natsandoya@gmail.com>
2024-12-17 03:32:00 -0500

    feat(config): Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nsandoya.github.io/samaritana',
   integrations: [
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
+    sitemap()
   ],
+  output: 'static',
 });
