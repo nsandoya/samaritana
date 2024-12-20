d87f5fd449740441af26ce1d95b5beb7c99384e9
nsandoya <natsandoya@gmail.com>
2024-12-19 23:48:36 -0500

    refactor!(data and pages): Blog: Use Nano store as single data source for posts and dynamic pages
    Originally, both posts.astro and blog/post/[slug].astro were set to call (individually) the same internal API. Now, dynamic pages like blog/post/[slug].astro rely on the  Nano store, improving maintainability and efficiency.

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,30 +1,52 @@
 
 import {fetchData} from './fetchData'
+import {blogPostsStore} from '@/store/posts'
+
 import type { BlogPost } from '@/interfaces/BlogPost';
 
-export async function getBlogPosts() {
-    const fields = {
+const fields = {
+  populate: {
+    featuredImage: {
+      fields: ["name", "width", "height", "url"],
+    },
+    author: {
       populate: {
-        featuredImage: {
+        bioImage: {
           fields: ["name", "width", "height", "url"],
         },
-        author: {
-          populate: {
-            bioImage: {
-              fields: ["name", "width", "height", "url"],
-            },
-          },
-        },
-        category: {
-          populate: true,
-        },
       },
-    }
-    
+    },
+    category: {
+      populate: true,
+    },
+  },
+}
+
+export async function getBlogPosts() {
+    // Get blog posts
     const posts:any = await fetchData(fields, "posts");
-  
+    // Set data into blog store
+    blogPostsStore.set(posts.body);
+
     return {
       body: posts.body as BlogPost[],
     };
+}
+
+export async function getPostByID(id:string) {
+    const post:any = await fetchData(fields, `posts/${id}`);
+
+    return {
+      body: post.body as BlogPost 
+    };   
+}
+
+export async function initializePostsStore() {
+  let  posts = blogPostsStore.get();
+  if (!posts || posts.length === 0) {
+    const { body } = await getBlogPosts() as any
+    blogPostsStore.set(body);
+    posts = blogPostsStore.get()
   }
-  
+  return posts;
+}
